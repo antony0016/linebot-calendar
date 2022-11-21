081e18c266c9da98d8839b349d565184222419d4
sekixu <feather881030@gmail.com>
2022-11-21 23:17:13 +0800

    Fix: change default title and description to blank.

--- model/todo.py
+++ model/todo.py
@@ -155,8 +155,8 @@ class EventSetting(Base):
 
     is_group = Column(Boolean, default=False)
     group_id = Column(String, nullable=True)
-    title = Column(String, default='未設定標題')
-    description = Column(String, default='未設定敘述')
+    title = Column(String, default='')
+    description = Column(String, default='')
     start_time = Column(DateTime)
 
     def to_dict(self):
